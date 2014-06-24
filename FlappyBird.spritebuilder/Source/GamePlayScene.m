b9ddd2aba44976e377fea9184b15f6e6d0ce3571
Benny Wang <xjw@bennys-air.gateway.2wire.net>
2014-06-24 14:28:56 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -20,6 +20,7 @@
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
+    [character flap];
 }
 
 @end
