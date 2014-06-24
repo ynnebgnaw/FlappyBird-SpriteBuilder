c1c5d2c57b5e835e1dab5f05568895754a4cc24b
Benny Wang <xjw@bennys-air.gateway.2wire.net>
2014-06-24 14:32:44 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,7 +11,11 @@
     [physicsNode addChild:character];
     [self addObstacle];
     timeSinceObstacle = 0.0f;
-    
+    - (void)update:(CCTime)delta
+    {
+        // this will be run every frame.
+        // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    }
 }
 
 -(void)update:(CCTime)delta
